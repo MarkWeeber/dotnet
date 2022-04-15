66dae3653b5ea22a045f85659a587242fe78df26
Asylbek Karashev <asylbekka@asisnur.kg>
2022-04-15 11:21:24 +0600

    tasks seems complete

--- app1/Program.cs
+++ app1/Program.cs
@@ -6,10 +6,15 @@ namespace app
     {
         public static void Main(string[] args)
         {
+            // task # 1
+            Console.WriteLine("Hello World !!!");
+            Console.ReadKey();
+            Console.WriteLine();
+            // task # 2
             Console.Write("Hello ");
             Console.Write("World ");
             Console.Write("!!!");
-            Console.ReadLine();          
+            Console.ReadLine();
         }
     }
 }
