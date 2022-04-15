33a3f00a16215049bbce80cb61aa559975e71d63
Asylbek Karashev <asylbekka@asisnur.kg>
2022-04-15 11:10:12 +0600

    first app commit

---
+++ app1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace app
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+            Console.ReadLine();          
+        }
+    }
+}
