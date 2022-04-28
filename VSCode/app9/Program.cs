de8473aad36ca1256c6267c02f0e2be692abff55
Asylbek Karashev <asylbekka@asisnur.kg>
2022-04-28 10:48:49 +0600

    hw# 9 seems complete via vs code

--- VSCode/app9/Program.cs
+++ VSCode/app9/Program.cs
@@ -4,23 +4,24 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types.InputFiles;
 
 namespace app9
 {
     class Program
     {
-        private static TelegramBotClient client;
-        private static string saveDirectory;
-        private static string token;
+        private static TelegramBotClient Client;
+        private static string SaveDirectory;
+        private static string Token;
         static void Main(string[] args)
         {
-            saveDirectory = Directory.GetCurrentDirectory() + @"\save\";
-            token = File.ReadAllText(Directory.GetCurrentDirectory() + @"\telegramToken.txt");
-            client = new TelegramBotClient(token);
-            client.StartReceiving();
-            client.OnMessage += OnMessageHandler;
+            SaveDirectory = Directory.GetCurrentDirectory() + @"\save\";
+            Token = File.ReadAllText(Directory.GetCurrentDirectory() + @"\telegramToken.txt");
+            Client = new TelegramBotClient(Token);
+            Client.StartReceiving();
+            Client.OnMessage += OnMessageHandler;
             Console.ReadLine();
-            client.StopReceiving();
+            Client.StopReceiving();
         }
 
         private static async void OnMessageHandler(object sender, MessageEventArgs e)
@@ -32,39 +33,82 @@ namespace app9
             {
                 
                 case Telegram.Bot.Types.Enums.MessageType.Text:
-                prefix = "wrote:";
-                suffix = e.Message.Text;
+                    prefix = "wrote:";
+                    suffix = e.Message.Text;
                 break;
                 case Telegram.Bot.Types.Enums.MessageType.Document:
-                prefix = "sent a document, saved with name:";
-                suffix = e.Message.Document.FileName;
-                DownloadFile(e.Message.Document.FileId, e.Message.Document.FileName);
+                    prefix = "sent a document, saved with name:";
+                    suffix = e.Message.Document.FileName;
+                    DownloadFile(e.Message.Document.FileId, e.Message.Document.FileName);
                 break;
                 case Telegram.Bot.Types.Enums.MessageType.Photo:
-                prefix = "sent a photo, saved with name:";
-                suffix = "photo_" + e.Message.Photo.FileId;
-                DownloadFile(e.Message.Photo.FileId, suffix);
-                break;
+                    prefix = "sent a photo, saved with name:";
+                    suffix = "photo_" + e.Message.MessageId + ".png";
+                    DownloadFile(e.Message.Photo[e.Message.Photo.Length - 1].FileId, suffix);
+                    break;
+                case Telegram.Bot.Types.Enums.MessageType.Voice:
+                    prefix = "sent a audio message, saved with name:";
+                    suffix = "audio_" + e.Message.MessageId + ".mp3";
+                    DownloadFile(e.Message.Voice.FileId, suffix);
+                    break;
                 default: break;
             }
             string text = $"{DateTime.Now} : {e.Message.Chat.FirstName} {e.Message.Chat.LastName} {prefix} {suffix}";
             Console.WriteLine(text);
+            // greeting
+            if(e.Message.Text.Trim().ToLower() == "hi")
+            {
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "Hi, this is telegram bot, you can send me messages, I will save any documment, photo and voice message you send me, if you type /files - I will list all files I saved");
+            }
             // checking for /start command
             if(e.Message.Text == "/start")
             {
-                string response = "Already started";
-                await client.SendTextMessageAsync(e.Message.Chat.Id, response);
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "Already started");
+            }
+            // listing present files in save directory to user
+            if(e.Message.Text == "/files")
+            {
+                if (!Directory.Exists(SaveDirectory) || !Directory.EnumerateFileSystemEntries(SaveDirectory).Any())
+                {
+                    await Client.SendTextMessageAsync(e.Message.Chat.Id, "No files saved");
+                }
+                else
+                {
+                    DirectoryInfo di = new DirectoryInfo(SaveDirectory);
+                    FileInfo[] filesInDirectory = di.GetFiles("*.*");
+                    if(filesInDirectory.Length > 0)
+                    {
+                        await Client.SendTextMessageAsync(e.Message.Chat.Id, "Here are some files saved in my directory");
+                    }
+                    else
+                    {
+                        await Client.SendTextMessageAsync(e.Message.Chat.Id, "No files saved");
+                    }
+                    foreach (var item in filesInDirectory)
+                    {
+                        string filePath = SaveDirectory + item.Name;
+                        using (FileStream fs = File.Open(filePath, FileMode.Open))
+                        {
+                            InputOnlineFile file = new InputOnlineFile(fs);
+                            file.FileName = item.Name;
+                            await Client.SendDocumentAsync(e.Message.Chat.Id, file, "MESSAGE");
+                        }
+                    }
+                }
             }
         }
 
         private static async void DownloadFile(string fileId, string fileName)
         {
-            var file = await client.GetFileAsync(fileId);
-            FileStream fileStream = new FileStream(saveDirectory + fileName, FileMode.Create);
-            await client.DownloadFileAsync(file.FilePath, fileStream);
+            var file = await Client.GetFileAsync(fileId);
+            if (!Directory.Exists(SaveDirectory))
+            {
+                Directory.CreateDirectory(SaveDirectory);
+            }
+            FileStream fileStream = new FileStream(SaveDirectory + fileName, FileMode.Create);
+            await Client.DownloadFileAsync(file.FilePath, fileStream);
             fileStream.Close();
             fileStream.Dispose();
         }
-
     }
 }
