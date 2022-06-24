4b6380cb143b2980eb31590b2444fcb58ead5893
unknown <m.webber@inbox.ru>
2022-06-24 21:35:04 +0600

    hw app12 seems complete now

--- app12/app12/obj/Debug/netcoreapp3.1/App.g.cs
+++ app12/app12/obj/Debug/netcoreapp3.1/App.g.cs
@@ -47,7 +47,7 @@ namespace app12 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.16.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace app12 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.16.0")]
         public static void Main() {
             app12.App app = new app12.App();
             app.InitializeComponent();
